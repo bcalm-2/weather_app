0f9240a145079ac6b2ef2f78bc63d6bb1e781a2a
bcalm-2 <dwivedisrashti6@gmail.com>
2025-12-07 13:49:20 +0530

    Modified vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/weather-app/",
+  base: "/weather_app/",
   plugins: [react()],
 });
