ede8b811ec138626473bd9c2b6b748ef0a9661c3
Srashti Dwivedi <dwivedisrashti6@gmail.com>
2025-12-07 07:49:11 +0000

    Added CSS for weather app

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,51 +1,59 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "./WeatherApp.css";
 
 const WeatherApp = () => {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
 
-    const [city, setCity] = useState("");
-    const [weather, setWeather] = useState(null);
+  const API_KEY = "a88a7ac3f95caef31324147c7433a2d3";
 
-    const API_KEY = "a88a7ac3f95caef31324147c7433a2d3";
+  const getWeather = async () => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      );
 
+      const data = await res.json();
 
-    const getWeather = async () => {
-
-        try {
-            const res =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
-
-            const data = await res.json();
-
-            if (data.cod=== 200) {
-                setWeather(data)
-            }
-            else if (data.cod == 401){
-                setWeather(null);
-                alert("Bad Request");
-            } else {
-                setWeather(null);
-                alert("City not exist")
-            }
-        }
-        catch(error){
-            console.error("Error fetching weather:",error)
-        }
+      if (data.cod === 200) {
+        setWeather(data);
+      } else if (data.cod == 401) {
+        setWeather(null);
+        alert("Invalid API Key");
+      } else {
+        setWeather(null);
+        alert("City not found");
+      }
+    } catch (error) {
+      console.error("Error fetching weather:", error);
     }
-    return (
-        <>
-            <div>WeatherApp</div>
-            <input type='text' placeholder='Enter city name' value={city}
-                onChange={(e) => setCity(e.target.value)} />
-            <button onClick={getWeather}>Search</button>
+  };
 
-            {weather && (
-                <div>
-                    <h2>Temparature: {weather.main.temp}°C</h2>
-                    <h2>Humidity: {weather.main.humidity}</h2>
-                    <h2>Wind Speed: {weather.wind.speed}</h2>
-                </div>
-            )}
-        </>
-    )
-}
+  return (
+    <div className="weather-container">
+      <div className="weather-title">Weather App</div>
 
-export default WeatherApp
+      <input
+        type="text"
+        className="weather-input"
+        placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <button className="weather-btn" onClick={getWeather}>
+        Search
+      </button>
+
+      {weather && (
+        <div className="weather-result">
+          <h3>Temperature: {weather.main.temp}°C</h3>
+          <h3>Humidity: {weather.main.humidity}%</h3>
+          <h3>Wind Speed: {weather.wind.speed} m/s</h3>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherApp;
